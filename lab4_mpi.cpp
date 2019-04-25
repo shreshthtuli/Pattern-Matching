be36fabedc44ca735a55449322da4c0566a5a54e
Shreshth Tuli <shreshthtuli@gmail.com>
2019-04-25 17:21:03 +0530

    Bug fixes

--- lab4_mpi.cpp
+++ lab4_mpi.cpp
@@ -6,14 +6,16 @@
 
 #define MASTER 0
 
+using namespace std;
+
 void calcPi(int* pi_set, int* m_set, int* p_set, int num_patterns)
 {
 	for(int i = 0; i < num_patterns; i++)
-		pi_set[i] = min(p_set[i], ceil(float(m_set[i])/2));
+		pi_set[i] = min(p_set[i], (int)ceil(float(m_set[i])/2));
 }
 
-void pi(int period, int m){
-	return min(period, ceil(float(m)/2));
+int pi(int period, int m){
+	return min(period, (int)ceil(float(m)/2));
 }
 
 void calcWitness(int** witness, int* pi_set, char** pattern_set, int num_patterns)
@@ -29,14 +31,14 @@ void calcWitness(int** witness, int* pi_set, char** pattern_set, int num_pattern
 			}
 			phi[j] = k;
 		}
-		witness[i] = *phi;
+		witness[i] = phi;
 	}
 }
 
-void witn(int* witness, int pi, char* pattern)
+void witn(int** witness, int pi, char* pattern)
 {
 	int* phi = new int[pi];
-	phi[0] = 0;
+	phi[0] = 0; int j, k;
 	for(j = 1; j < pi; j++){
 		for(k = 0; k < pi - j; k++){
 			if(pattern[k] != pattern[j+k])
@@ -44,13 +46,13 @@ void witn(int* witness, int pi, char* pattern)
 		}
 		phi[j] = k;
 	}
-	witness = *phi;
+	*witness = phi;
 }
 
-int duel(char* Z, int n; char* pattern, int* phi, int i, int j)
+int duel(char* Z, int n, char* pattern, int* phi, int i, int j)
 {
 	int k = phi[j-i];
-	if((j+k < 0 || j+k > n) || Z[j+k] != Y[k])
+	if((j+k < 0 || j+k > n) || Z[j+k] != pattern[k])
 		return i;
 	return j;
 }
@@ -66,11 +68,13 @@ bool match(char* T, int i, char* p, int m)
 
 void npTextAnalysis(char* T, int n, char* p, int m, int* phi, int procs, int* match_count, int** matched_pos)
 {
-	int blocks = 12 / ceil(double(m)/2);
+	int b = n / ceil(float(m)/2);
 	int bi, first, last, j, i;
-	int* potential_pos = new int[blocks];
-	int* matched_pos_temp = new int[blocks];
+	int* potential_pos = new int[b];
+	int* matched_pos_temp = new int[b];
 	int count = 0;
+
+	cout << "Text=" << T << ", p=" << p << endl;
 
 	// Parallelise across procs
 	for(bi = 0; bi < b; bi++)
@@ -78,35 +82,49 @@ void npTextAnalysis(char* T, int n, char* p, int m, int* phi, int procs, int* ma
 		first = n*bi/b; last = n*(bi+1)/b;
 		i = first;
 		for(j = i+1; j < last; j++){
-			i = duel(T, p, phi, i, j);
+			i = duel(T, n, p, phi, i, j);
+			cout << "duel(" << i << "," << j << ") = " << i << endl;
 		}
 		potential_pos[bi] = i;
 	}
 
-	
-	for(i = 0; i  blocks; i++){
-		if(matched(T, i, p, m)){
-			matched_pos[count] = i;
+	for(int i = 0; i < n; i++)
+		cout << potential_pos[i] << " ";
+	cout << endl;
+
+	for(i = 0; i < b; i++){
+		if(match(T, potential_pos[i], p, m)){
+			matched_pos_temp[count] = potential_pos[i];
 			count++;
 		}
 	}
 
-	*match_counts = count;
+	*match_count = count;
 	*matched_pos = matched_pos_temp;
 }
 
 void pTextAnalysis(char* T, int n, char* p, int m, int period, int procs)
 {
-	char* pPrime = new char[2*p-1];
-	for(int i = 0; i < 2*p-1; i++)
+	char* pPrime = new char[2*period-1];
+	for(int i = 0; i < 2*period-1; i++)
 		pPrime[i] = p[i];
 	
+	cout << p << " " << pPrime << " ";
+	for(int i = 0; i < 2*period-1; i++)
+		cout << pPrime[i];
+	cout << endl;
 	int ppi = pi(period, m);
 	int* witness = new int[ppi];
-	witn(witness, ppi, p);
+	witn(&witness, ppi, pPrime);
+
+	cout << witness[0] << " " << witness[1] << " - " << ppi << endl;
 	int count, *pos;
 
-	npTextAnalysis(T, n, p, m, witness, procs, &count, &pos);
+	npTextAnalysis(T, n, pPrime, 2*period-1, witness, procs, &count, &pos);
+
+	for(int i = 0; i < count; i++)
+		cout << pos[i] << " ";
+	cout << "- " << count << endl;
 
 	char* u = new char[period];
 	for(int i = 0; i < period; i++)
@@ -130,9 +148,38 @@ void pTextAnalysis(char* T, int n, char* p, int m, int period, int procs)
 	int i;
 	for(int j = 0; j < count; j++){
 		i = pos[j];
-		if(match(T, i, u2v, 2*period+(m-k*preiod)))
+		if(match(T, i, u2v, 2*period+(m-k*period)))
 			M[i] = 1;
 	}
+
+	int* S = new int[n/period];
+	int** C = new int*[period];
+	for(int i = 0; i < n; i+=period)
+		S[i/period] = M[i];
+
+	for(int i=0; i < period; i++){
+		C[i] = new int[n/period];
+		for(int j = 0; j < n/period; j++){
+			C[i][j] = 1;
+			for(int p = j; p < j+k; p++){
+				if(S[p] == 0){
+					C[i][j] = 0;
+					break;
+				}
+			}
+		}
+	}
+
+	int* Match = new int[n-m];
+	for(int i = 0; i < period; i++){
+		for(int l = 0; l < n/period; l++){
+			if(i+l*period < n-m+1)
+				Match[i+l*period] = C[i][l];
+		}
+	}
+
+	for(int i = 0; i < n-m+1; i++)
+		cout << "M[" << i << "] = " << M[i] << endl;
 }
 
 void periodic_pattern_matching (
@@ -152,16 +199,21 @@ void periodic_pattern_matching (
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	std::cout << "Numprocs = " << procs << " rank = " << rank << std::endl;
-	std::cout << "first = " << first << " last = " << last-1 << std::endl;
+	// std::cout << "first = " << first << " last = " << last-1 << std::endl;
 
 	int* counts = new int[num_patterns];
 	int* pi_set = new int[num_patterns];
 	int** witness = new int*[num_patterns];
 
-	calcPi(pi_set, m_set, p_set, num_patterns);
-	calcWitness(witness, pi_set, pattern_set, num_patterns);
+	// calcPi(pi_set, m_set, p_set, num_patterns);
+	// calcWitness(witness, pi_set, pattern_set, num_patterns);
 	
 	int i, j, k; bool matched;
+
+	for(int i = 0; i < num_patterns; i++){
+		cout << "n = " << n << " m = " << m_set[i] << " period = " << p_set[i] << endl;
+		pTextAnalysis(text, n, pattern_set[i], m_set[i], p_set[i], procs);
+	}
 	
 	MPI_Finalize();
 	*match_counts = counts;
